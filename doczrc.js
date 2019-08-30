ab30977e2aa06fb0800293d97fb5d75b89bc00cd
Levi Thomason <me@levithomason.com>
2019-08-30 11:46:58 -0700

    fix: routes and pages

--- doczrc.js
+++ doczrc.js
@@ -4,4 +4,13 @@ import images from 'remark-images'
 export default {
   title: 'Open UI',
   mdPlugins: [images, emoji],
+  ignore: [
+    'README.md',
+    'CHANGELOG.md',
+    'CODE_OF_CONDUCT.md',
+    'CONTRIBUTING.md',
+    'LICENSE.md',
+    'LICENSING.md',
+    'TODO.md'
+  ],
 }
