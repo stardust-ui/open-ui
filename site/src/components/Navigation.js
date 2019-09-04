9e356c1d7abd2c35c2cd5b4c8e4c36c0d5a91946
Levi Thomason <me@levithomason.com>
2019-09-04 15:58:31 -0700

    add home link

--- site/src/components/Navigation.js
+++ site/src/components/Navigation.js
@@ -22,14 +22,46 @@ const Navigation = ({ style }) => (
     `}
     render={data => {
       // get all frontmatter objects with a menu defined
-      const nodes = _.map(data.allMdx.edges, 'node.frontmatter').filter(({ menu }) => !!menu)
+      const [menuNodes, topLevelNodes] = _.partition(_.map(data.allMdx.edges, 'node.frontmatter').filter(({ name }) => !!name), 'menu')
 
       // get all frontmatter objects with a menu defined
-      const menu = _.sortBy(_.toPairs(_.groupBy(nodes, 'menu')), _.first)
+      const menu = _.sortBy(_.toPairs(_.groupBy(menuNodes, 'menu')), _.first)
+
+      let listItem = item => (
+        <li
+          key={item.name}
+          style={{
+            margin: 0,
+            listStyleType: 'none',
+          }}
+        >
+          <Link
+            to={item.path}
+            style={{
+              display: 'block',
+              padding: '0.25em 1em 0.25em 0.5em',
+              margin: 0,
+              borderLeft: '2px solid transparent',
+              borderRadius: '2px',
+              whiteSpace: 'nowrap',
+            }}
+            activeStyle={{
+              borderLeftColor: '#00a453',
+              backgroundColor: '#f2f2f2',
+            }}
+          >
+            {item.name}
+          </Link>
+        </li>
+      )
 
       return (
         <nav style={style}>
           <ul style={{ position: 'sticky', top: '1em', margin: 0 }}>
+            {topLevelNodes.map(listItem)}
+
+            <div style={{ margin: '1rem'}} />
+
             {menu.map(([category, items]) => (
               <li key={category} style={{ margin: 0, listStyleType: 'none' }}>
                 <div
@@ -42,33 +74,7 @@ const Navigation = ({ style }) => (
                   {category}
                 </div>
                 <ul style={{ margin: '0.25em 0 1.5em 0' }}>
-                  {items.map(item => (
-                    <li
-                      key={item.name}
-                      style={{
-                        margin: 0,
-                        listStyleType: 'none',
-                      }}
-                    >
-                      <Link
-                        to={item.path}
-                        style={{
-                          display: 'block',
-                          padding: '0.25em 1em 0.25em 0.5em',
-                          margin: 0,
-                          borderLeft: '2px solid transparent',
-                          borderRadius: '2px',
-                          whiteSpace: 'nowrap',
-                        }}
-                        activeStyle={{
-                          borderLeftColor: '#00a453',
-                          backgroundColor: '#f2f2f2',
-                        }}
-                      >
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))}
+                  {items.map(listItem)}
                 </ul>
               </li>
             ))}
